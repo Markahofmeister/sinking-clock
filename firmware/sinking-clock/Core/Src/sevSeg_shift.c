857047545e65a720ca06989ae8ec758bd4d3c264
markahofmeister <markhof12@gmail.com>
2023-07-23 04:54:44 -0400

    Add beginnings of new shift register code

---
+++ firmware/sinking-clock/Core/Src/sevSeg_shift.c
@@ -0,0 +1,21 @@
+/*
+ * sevSeg_shift.c
+ *
+ *  Created on: Jul 23, 2023
+ *      Author: marka
+ */
+
+#include "../Inc/sevSeg.h"
+
+/*
+ * Flashes an initializing "Hof" symbol
+ */
+
+void sevSeg_init() {
+
+}
+
+void sevSeg_updateDigits() {
+
+}
+
