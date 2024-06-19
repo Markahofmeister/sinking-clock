2917c2a5243c208dcc665244cb848554d7491c47
markahofmeister <markhof12@gmail.com>
2024-06-19 08:21:40 -0400

    Worked beautifully.

--- firmware/sinking-clock_master/Core/Src/main.c
+++ firmware/sinking-clock_master/Core/Src/main.c
@@ -601,11 +601,11 @@ static void MX_TIM16_Init(void)
 
   /* USER CODE END TIM16_Init 1 */
   htim16.Instance = TIM16;
-  htim16.Init.Prescaler = 58595 / (600 / 20 * 10) - 1;
+  htim16.Init.Prescaler = (58595 / 10) - 1;
   htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim16.Init.Period = 65535;
   htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim16.Init.RepetitionCounter = 100;
+  htim16.Init.RepetitionCounter = timerSnooze_RCR;
   htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim16) != HAL_OK)
   {
